e004b2a830db35fabc457ffd2fba3c2000889fa8
PushparajNt <pushparajcsegce@gmail.com>
2022-07-28 09:24:49 +0530

    Added RequestBody annotation

--- mssc-brewery/src/main/java/com/sfg/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/com/sfg/web/controller/BeerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +33,7 @@ public class BeerController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<CustomerDto> addNewBeer(BeerDto beerDto)
+	public ResponseEntity<CustomerDto> addNewBeer(@RequestBody BeerDto beerDto)
 	{
 		BeerDto savedDto=beerService.addNewBeer(beerDto);
 		HttpHeaders httpHeaders=new HttpHeaders();
